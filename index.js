4cea8b8964fd9ffbdd6f3022be3481f6a9eb610d
Amr ElGohary <73877818+xamrx700@users.noreply.github.com>
2021-02-24 06:00:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ image1.setAttribute("src", randomDiceImage1);
 var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 
 if(randomNumber1 > randomNumber2){
-document.querySelector("h1").innerHTML = "😎Amr is The WINNER!"
+document.querySelector("h1").innerHTML = " 🤸‍♂️ Amr is The WINNER!"
 }
 else if(randomNumber1 < randomNumber2){
-document.querySelector("h1").innerHTML = "Poki is The WINNER!💋"
+document.querySelector("h1").innerHTML = "Poki is The WINNER!💃"
 }
 else{
 document.querySelector("h1").innerHTML = "DRAW!"
